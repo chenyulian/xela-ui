5908a3c61ae7c132f693c3fae898a60e685bf86f
kinppink <648270560@qq.com>
2021-07-08 10:43:20 +0800

    :lipstick:更新topnav样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,13 @@
 <template>
     <div>
         <div class="topnav">
-            <div class="log"></div>
-            <div class="menu"></div>
+            <div class="log">LOGO</div>
+            <div class="menu">
+                <ul class="menu">
+                    <li>菜单1</li>
+                    <li>菜单2</li>
+                </ul>
+            </div>
         </div>
         <div class="banner">
             <h1>Xela UI</h1>
@@ -22,5 +27,24 @@
 </script>
 
 <style lang="scss" scoped>
+.topnav {
+    background: pink;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 16px;
+    & .logo {
+        max-width: 6em;
+        margin-right: auto;
+    }
 
+    & .menu {
+        display: flex;
+        white-space: nowrap;
+        flex-wrap: nowrap;
+        > li {
+            margin: 0 1em;
+        }
+    }
+}
 </style>
