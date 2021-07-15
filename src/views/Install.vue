b320ddd64f4684c38b456e614802f7953aa7cd6b
kinppink <648270560@qq.com>
2021-07-15 11:41:15 +0800

    :memo:更新文档

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,7 +1,15 @@
 <template>
-    <div>
-        install
-    </div>
+    <article>
+        <h1>安装</h1>
+        <p>打开终端运行下列命令：</p>
+        <pre><code>npm install xela-ui</code></pre>
+        <p>或</p>
+        <pre><code>yarn add xela-ui</code></pre>
+
+        <p>
+            下一节：<a href="#/doc/get-started">开始使用</a>
+        </p>
+    </article>
 </template>
 
 <script>
--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,7 +1,10 @@
 <template>
-    <div>
-        intro
-    </div>
+    <article>
+        <h1>介绍</h1>
+        <p>Xela UI 是一款小型UI组件库，基于Vue 3和TypeScript。</p>
+        <p>此UI库是作者为了学习Vue 3而编写的一个练手项目，目前还未发布到npm。</p>
+        <p>此项目仅供学习，请勿用于生产环境。</p>
+    </article>
 </template>
 
 <script>
