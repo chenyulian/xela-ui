39d409ee8c3b4d5888e7313808e9f7b79e9786d1
kinppink <648270560@qq.com>
2021-07-15 09:49:03 +0800

    :construction:添加路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,9 @@
 import {createWebHashHistory, createRouter} from "vue-router";
 import Home from "../views/Home.vue";
 import Doc from "../views/Doc.vue";
+import Intro from "../views/Intro.vue";
+import Install from "../views/Install.vue";
+import GetStarted from "../views/GetStarted.vue";
 import SwitchDemo from "../components/demo/SwitchDemo.vue";
 import DocDemo from "../components/demo/DocDemo.vue";
 import ButtonDemo from "../components/demo/ButtonDemo.vue";
@@ -13,6 +16,7 @@ import LabelDemo from "../components/demo/LabelDemo.vue";
 import AlertDemo from "../components/demo/AlertDemo.vue";
 import TabsDemo from "../components/demo/TabsDemo.vue";
 
+
 const history = createWebHashHistory();
 export const router = createRouter({
     history: history,
@@ -20,6 +24,9 @@ export const router = createRouter({
         {path: '/', component: Home},
         {path: "/doc", component: Doc, children: [
             {path: "", component: DocDemo},
+            {path: "intro", component: Intro},
+            {path: "install", component: Install},
+            {path: "get-started", component: GetStarted},
             {path: "button", component: ButtonDemo},
             {path: "switch", component: SwitchDemo},
             {path: "dialog", component: DialogDemo},
