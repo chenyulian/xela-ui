f56f21d102b78674ce7b20d4b9e183657e6c640b
kinppink <648270560@qq.com>
2021-07-15 15:26:39 +0800

    抽离demo代码

--- src/components/demo/SwitchDemo.vue
+++ src/components/demo/SwitchDemo.vue
@@ -1,14 +1,28 @@
 <template>
     <div>
-        <Switch v-model:value="checked" />
+        <h1>Switch组件示例</h1>
+        <div class="demo">
+            <h2>基础用法</h2>
+            <div class="demo-component">
+                <SwitchDemo1 />
+            </div>
+            <div class="demo-actions">
+                <Button>查看代码</Button>
+            </div>
+            <div class="demo-code">
+                <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+            </div>
+        </div>
     </div>
 </template>
 <script lang="ts">
 import Switch from "../../lib/Switch.vue";
+import Button from "../../lib/Button.vue";
+import SwitchDemo1 from "./SwitchDemo1.vue";
 import {ref} from "vue";
 export default {
     components: {
-        Switch
+        Switch, Button, SwitchDemo1
     },
     setup() {
         const checked = ref(false);
@@ -17,5 +31,30 @@ export default {
 }
 </script>
 <style lang="scss">
-    
+$border-color: #efeff5;
+.demo {
+  border: 1px solid $border-color;
+  margin: 16px 0 32px;
+  >h2 {
+    font-size: 20px;
+    padding: 8px 16px;
+    border-bottom: 1px solid $border-color;
+  }
+  &-component {
+    padding: 16px;
+  }
+  &-actions {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+  }
+  &-code {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+    >pre {
+      line-height: 1.1;
+      font-family: Consolas, 'Courier New', Courier, monospace;
+      margin: 0;
+    }
+  }
+}
 </style>
---
+++ src/components/demo/SwitchDemo1.vue
@@ -0,0 +1,15 @@
+<template>
+    <Switch v-model:value="checked" />
+</template>
+
+<script lang="ts">
+    import { ref } from "vue";
+    import Switch from "../../lib/Switch.vue";
+    export default {
+        components: { Switch }, 
+        setup() {
+            const checked = ref(false);
+            return { checked };
+        }
+    }
+</script>
