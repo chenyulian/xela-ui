eb46d6d20916399f6a08ae8b568f741ab32deb41
kinppink <648270560@qq.com>
2021-08-25 16:11:42 +0800

    :memo:修改部署配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import fs from 'fs'
 import {baseParse} from '@vue/compiler-core'
 
 export default {
+  base: './',
+  assetsDir: 'assets',
   vueCustomBlockTransforms: {
     demo: (options) => {
       const { code, path } = options
